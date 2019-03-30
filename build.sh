af430e9bd8f00b72a25d524be5f2bb435a60d725
TheAssassin <theassassin@assassinate-you.net>
2019-03-30 17:36:11 +0100

    Initial commit

---
+++ build.sh
@@ -0,0 +1,57 @@
+#! /bin/bash
+
+#! /bin/bash
+
+set -x
+set -e
+
+# use RAM disk if possible
+if [ "$BIONIC" == "" ] && [ "$COSMIC" == "" ] && [ -d /dev/shm ]; then
+    TEMP_BASE=/dev/shm
+else
+    TEMP_BASE=/tmp
+fi
+
+BUILD_DIR=$(mktemp -d -p "$TEMP_BASE" AppImageLauncher-build-XXXXXX)
+
+cleanup () {
+    if [ -d "$BUILD_DIR" ]; then
+        rm -rf "$BUILD_DIR"
+    fi
+}
+
+trap cleanup EXIT
+
+# store repo root as variable
+REPO_ROOT=$(readlink -f $(dirname $(dirname $0)))
+OLD_CWD=$(readlink -f .)
+
+pushd "$BUILD_DIR"
+
+mkdir AppDir
+touch AppDir/example.png
+cat > AppDir/example.desktop <<\EOF
+[Desktop Entry]
+Name=example
+Icon=example
+Exec=example
+Type=Application
+Categories=Development;
+Terminal=true
+EOF
+
+cat > AppDir/AppRun <<\EOF
+#! /bin/bash
+echo "It's ALIVE!!!!111elf"
+EOF
+
+chmod +x AppDir/AppRun
+
+wget https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage
+
+chmod +x appimagetool-x86_64.AppImage
+
+export ARCH=x86_64
+./appimagetool-x86_64.AppImage -g AppDir
+
+./example*.AppImage
